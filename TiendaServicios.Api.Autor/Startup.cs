30f3d5ab011e76d142889ff55ac53182468cbde3
Carlos <carlospc402@gmail.com>
2023-03-14 12:34:56 -0600

    MS postgresql, sql server y mysql

---
+++ TiendaServicios.Api.Autor/Startup.cs
@@ -0,0 +1,68 @@
+using AutoMapper;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Aplicacion;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Similar al mediator no hay que escribir una linea por cada clase, al iniciarse el proyecto reconoce todas las clases
+            //que heredan AbstractValidator
+            services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
+            services.AddDbContext<ContextoAutor>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("ConexionDatabase"));
+            });
+
+            //No es necesario escribir una por una todas las clases, al iniciarse el proyecto va a reconocer de que se 
+            //está usando las interfaces de mediatR y en ese momento empezará a buscar todas las clases heredades de
+            //MediatR (las que heredan IRquest e IRequestHandler)
+            services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
+
+            //Mismo caso no es necesario agregar un línea por cada clase
+            services.AddAutoMapper(typeof(Consulta.Manejador));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
